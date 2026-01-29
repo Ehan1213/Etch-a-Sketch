bad1b6678c9126725a858be2bd872cbdac6bfe51
Ian L <187871945+Ehan1213@users.noreply.github.com>
2026-01-29 15:42:43 -0500

    Add drawing function
    Either the flexbox layout is not working properly, or the drawing needs
    handled in a different way. It does draw the divs though.

--- etchASketch.js
+++ etchASketch.js
@@ -4,13 +4,27 @@ const container = document.querySelector(".container")
 redrawButton.addEventListener("click", () => {
     let defaultInput = 16;
     userInput = prompt(`Input amount of boxes per side. \nThe default is 16`);
-    if (userInput === null){
+    if (userInput === ""){
         drawGrid(defaultInput)
+    } else if (userInput > 100){
+        alert(`${userInput} by ${userInput} is too many boxes!`)
     }
+    
     drawGrid(userInput)
 });
 
 function drawGrid(num){
-    
+    grid = num ** 2 
+    for(let i = 0; i < grid; i++){
+        const box = document.createElement("div")
+        box.classList.add("box")
+        container.appendChild(box)
+    }
 
 };
+
+
+window.onload = function() {
+  drawGrid(16);
+};
+   
