c4ea25d76b35328587a1b8cfc05df1befd5f0b78
Ian L <187871945+Ehan1213@users.noreply.github.com>
2026-01-29 22:53:49 -0500

    Add fix to redrawing squares based on user input.
    I DID IT YES!

--- etchASketch.js
+++ etchASketch.js
@@ -1,28 +1,34 @@
-const redrawButton = document.querySelector("#redraw")
-const content = document.querySelector(".content")
+"use strict";
+const redrawButton = document.querySelector("#redraw");
+const content = document.querySelector(".content");
 
 redrawButton.addEventListener("click", () => {
     let defaultInput = 16;
-    let redraw = true
-    userInput = prompt(`Input amount of boxes per side. \nThe default is 16`);
+    let bool = true;
+    let userInput = prompt(`Input amount of boxes per side. \nThe default is 16`);
     if (userInput === ""){
-        drawGrid(defaultInput)
+        drawGrid(defaultInput);
     } else if (userInput > 100){
-        alert(`${userInput} by ${userInput} is too many boxes!`)
+        alert(`${userInput} by ${userInput} is too many boxes!`);
     }
-    drawGrid(userInput, redraw)
+    else {drawGrid(userInput, bool);}
 });
 
 
 
-function drawGrid(num, reDrawBool = false){
-    reDraw = reDrawBool
+function drawGrid(num, bool = false){
+    let reDraw = bool
 
-    if (redraw === true){
-        row.removeChild(box)
-        content.removeChild(row)
-    }
+    if (reDraw === true){
+        let rowList = document.querySelectorAll(`.row`);
+        for(let i = 0; i < rowList.length; i++){
+            let rowRemove = rowList[i]
+            content.removeChild(rowRemove)
+
+        }
         
+    }
+
     for(let i = 0; i < num; i++){
         let row = document.createElement("div");
         row.classList.add("row");
@@ -30,24 +36,68 @@ function drawGrid(num, reDrawBool = false){
         content.appendChild(row);
 
         let currentRow = document.querySelector(`#row${i}`);
+        let rowList = document.querySelectorAll('.row');
         content.appendChild(currentRow);
 
+        console.log(rowList);
+        console.log(currentRow);
+        console.log("row.id= " + row.id);
+        console.log(`i= ${i}`);
+        console.log(`num= ${i}`);
+        console.log(`#row= ${i}`);
+        console.log(`querySelector= #row${i}`);
+
+        
+        
         for(let j = 0; j < num; j++){
             let box = document.createElement("div");
             box.classList.add("box");
             currentRow.appendChild(box);
         }
         content.appendChild(currentRow);
-        
-        
-    }
 
+
+    }
 };
 
-function clearGrid(){
 
-}
 
+
+
+
+
+
+
+
+
+// function drawGrid(num, reDrawBool = false){
+//     reDraw = reDrawBool
+
+//     if (redraw === true){
+//         row.removeChild(box)
+//         content.removeChild(row)
+//     }
+        
+//     for(let i = 0; i < num; i++){
+//         let row = document.createElement("div");
+//         row.classList.add("row");
+//         row.id = `row${i}`;
+//         content.appendChild(row);
+
+//         let currentRow = document.querySelector(`#row${i}`);
+//         // content.appendChild(currentRow);
+
+//         for(let j = 0; j < num; j++){
+//             let box = document.createElement("div");
+//             box.classList.add("box");
+//             currentRow.appendChild(box);
+//         }
+//         content.appendChild(currentRow);
+        
+        
+//     }
+
+// };
 
 
 const rowCount = document.querySelectorAll(".row");
@@ -60,9 +110,7 @@ const test = document.createElement("p")
 
 
 window.onload = function() {
-  drawGrid(16);
-  test
-  content.appendChild(test)
+  drawGrid(15);
   
 };
 
